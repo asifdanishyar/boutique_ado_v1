892e18f60add81a00e2af40e9339d47346499083
asifdanishyar <asifdanishyar@yahoo.com>
2020-10-17 08:38:17 +0000

    updated allauth templates and profiles

--- profiles/models.py
+++ profiles/models.py
@@ -7,10 +7,14 @@ from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label= 'Country *', null=True, blank=True)
-    default_default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
@@ -21,11 +25,11 @@ class UserProfile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def createt_or_update_user_profile(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    create or update the user profile
+    Create or update the user profile
     """
     if created:
         UserProfile.objects.create(user=instance)
-    # existing users: just save the profile
-    instance.UserProfile.save()
+    # Existing users: just save the profile
+    instance.userprofile.save()
