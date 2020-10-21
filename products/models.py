78515afc6156385af5daa106955894b164d05fb3
asifdanishyar <asifdanishyar@yahoo.com>
2020-10-21 18:20:20 +0000

    finished add product functionality

--- products/models.py
+++ products/models.py
@@ -5,9 +5,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-
+        
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
